74153f64ed11b01e3546dbb7084407299a669247
Semen Burov <rileys@bk.ru>
2025-11-20 00:31:56 +0300

    add test_decorators and fix decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -10,22 +10,28 @@ def log(filename=None):
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{func.__name__} ok\nРезультат: {result}\n"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message)
+                else:
+                    print(log_message)
+                return result
             except Exception as e:
-                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n"
-            if filename:
-                with open(filename, "a", encoding="utf-8") as file:
-                    file.write(log_message)
-            else:
-                print(log_message)
+                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message)
+                else:
+                    print(log_message)
 
         return wrapper
 
     return decorator
 
 
-@log("mylog.txt")
+@log()
 def my_function(x, y):
     return x + y
 
 
-my_function(10, 2)
+my_function(5, 6)
