c5be9cd1061ef2822816be7f6cc5fa70ef288349
Semen Burov <rileys@bk.ru>
2025-12-14 23:10:26 +0300

    fix flake8

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
-from src.transaction_reader import trans_reader_csv, path_file_csv, trans_reader_excel, path_file_excel
+from src.transaction_reader import path_file_csv, path_file_excel, trans_reader_csv, trans_reader_excel
 from src.widget import get_date, mask_account_card
 
 some_dict = [
@@ -9,14 +9,14 @@ some_dict = [
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
-# if __name__ == "__main__":
-    # print(get_mask_card_number(7000792289606361))
-    # print(get_mask_account(73654108430135874305))
-    # print(mask_account_card("Visa Platinum 7000792289606361"))
-    # print(mask_account_card("Maestro 7000792289606361"))
-    # print(mask_account_card("Счет 73654108430135874305"))
-    # print(get_date("2024-03-11T02:26:18.671407"))
-    # print(filter_by_state(some_dict))
-    # print(sort_by_date(some_dict))
-    # print(trans_reader_csv(path_file_csv))
-    # print(trans_reader_excel(path_file_excel))
+if __name__ == "__main__":
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(73654108430135874305))
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
+    print(filter_by_state(some_dict))
+    print(sort_by_date(some_dict))
+    print(trans_reader_csv(path_file_csv))
+    print(trans_reader_excel(path_file_excel))
